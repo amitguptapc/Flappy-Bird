33d57ca3f1263f3a20dff4327169bbc8273c4bb5
Amit Kumar Gupta <amitguptapc@gmail.com>
2018-05-01 01:31:00 +0530

    Added power ups and coins

--- src/Game/Demo.java
+++ src/Game/Demo.java
@@ -16,20 +16,18 @@ public class Demo implements KeyListener {
   private static boolean started = false;
   private static boolean paused = false;
   private static boolean lost = false;
-  private static int hurdle1XCord;
-  private static int hurdle2XCord;
-  private static int hurdle3XCord;
-  private static int hurdle4XCord;
-  private static int hurdle1YCord;
-  private static int hurdle2YCord;
-  private static int hurdle3YCord;
-  private static int hurdle4YCord;
+  private static int hurdle1XCord,hurdle1YCord,hurdle2XCord,hurdle2YCord,hurdle3XCord,hurdle4XCord,hurdle3YCord,hurdle4YCord;
+  private static int starXCord=1000;
+  private static int starYCord,coinYCord,coinXCord;
   private static int score = 0;
+  private static int counter=60;
+  private static boolean isPlayerVisible=true,coinDisplay,starVisible;
   private static Image hurdle[] = new Image[4];
-  private static Rectangle hurdle1Rect, hurdle2Rect, hurdle3Rect, hurdle4Rect;
+  private static Rectangle hurdle1Rect, hurdle2Rect, hurdle3Rect, hurdle4Rect,starRect,coinRect;
   private static Image branch;
   private static Font font1 = new Font("Kristen ITC", Font.BOLD, 22);
   private static Font font2 = new Font("Kristen ITC", Font.BOLD, 18);
+  private static Font font3 = new Font("Kristen ITC", Font.PLAIN, 16);
 
   public static void main(String[] args) {
     JFrame frame = new JFrame();
@@ -44,7 +42,8 @@ public class Demo implements KeyListener {
     panel.requestFocus();
     Demo p = new Demo();
     panel.addKeyListener(p);
-    Image back = null;
+    Image back;
+    back = null;
     Image back1 = null;
     Image p1 = null;
     Image p2 = null;
@@ -54,12 +53,25 @@ public class Demo implements KeyListener {
     Image p6 = null;
     Image p7 = null;
     Image p8 = null;
+    Image pt1 = null;
+    Image pt2 = null;
+    Image pt3 = null;
+    Image pt4 = null;
+    Image pt5 = null;
+    Image pt6 = null;
+    Image pt7 = null;
+    Image pt8 = null;
     Image log = null;
     Image fork = null;
     Image forkRev = null;
     Image forkHandle = null;
     Image forkHandleRev = null;
     Image controls = null;
+    Image star = null;
+    Image coin1 = null;
+    Image coin2 = null;
+    Image coin3 = null;
+    Image coin4 = null;
 
 
     try {
@@ -73,6 +85,19 @@ public class Demo implements KeyListener {
       p6 = ImageIO.read(Demo.class.getResource("../images/pappu_6.png"));
       p7 = ImageIO.read(Demo.class.getResource("../images/pappu_7.png"));
       p8 = ImageIO.read(Demo.class.getResource("../images/pappu_8.png"));
+      pt1 = ImageIO.read(Demo.class.getResource("../images/pappu1.png"));
+      pt2 = ImageIO.read(Demo.class.getResource("../images/pappu2.png"));
+      pt3 = ImageIO.read(Demo.class.getResource("../images/pappu3.png"));
+      pt4 = ImageIO.read(Demo.class.getResource("../images/pappu4.png"));
+      pt5 = ImageIO.read(Demo.class.getResource("../images/pappu5.png"));
+      pt6 = ImageIO.read(Demo.class.getResource("../images/pappu6.png"));
+      pt7 = ImageIO.read(Demo.class.getResource("../images/pappu7.png"));
+      pt8 = ImageIO.read(Demo.class.getResource("../images/pappu8.png"));
+      star = ImageIO.read(Demo.class.getResource("../images/star.png"));
+      coin1 = ImageIO.read(Demo.class.getResource("../images/coin1.png"));
+      coin2 = ImageIO.read(Demo.class.getResource("../images/coin2.png"));
+      coin3 = ImageIO.read(Demo.class.getResource("../images/coin3.png"));
+      coin4 = ImageIO.read(Demo.class.getResource("../images/coin4.png"));
       forkHandle = ImageIO.read(Demo.class.getResource("../images/fork_handle.png"));
       forkHandleRev = ImageIO.read(Demo.class.getResource("../images/fork_handlerev.png"));
       Demo.branch = ImageIO.read(Demo.class.getResource("../images/branch.png"));
@@ -91,8 +116,11 @@ public class Demo implements KeyListener {
     hurdle[1] = branch;
     hurdle[2] = branch;
     hurdle[3] = branch;
-
+    starVisible=false;
     Image imageArray[] = {p1, p2, p3, p4, p5, p6, p7, p8};
+    Image tImageArray[] = {pt1, pt2, pt3, pt4, pt5, pt6, pt7, pt8};
+    Image coinArray[] = {coin1,coin2,coin3,coin4};
+    int scoreArray[]={50,100,200,500};
     Demo.hurdle1XCord = 1000;
     Demo.hurdle2XCord = 1000;
     Demo.hurdle3XCord = 1500;
@@ -101,10 +129,14 @@ public class Demo implements KeyListener {
     Demo.hurdle2YCord = 350;
     Demo.hurdle3YCord = -400;
     Demo.hurdle4YCord = 250;
+    starRect=new Rectangle(41,39);
+    coinRect=new Rectangle(30,30);
     Random ran = new Random();
     boolean forkOrBranch;
     boolean upOrDown;
-    int length;
+    coinDisplay=false;
+    int length,whichCoin=0;
+    Image coinShown=null;
     hurdle1Rect = new Rectangle(Demo.hurdle1XCord, Demo.hurdle1YCord, 31, 500);
     hurdle2Rect = new Rectangle(Demo.hurdle2XCord, Demo.hurdle2YCord, 31, 500);
     hurdle3Rect = new Rectangle(Demo.hurdle3XCord, Demo.hurdle3YCord, 31, 500);
@@ -133,6 +165,7 @@ public class Demo implements KeyListener {
       }
 
       count = (count + 1) % 8;
+
       g.drawImage(back, backxcord, 0, null);
       g.drawImage(back1, back1xcord, 0, null);
       g.drawImage(log, Demo.logYCord, 330, null);
@@ -140,19 +173,76 @@ public class Demo implements KeyListener {
       g.drawImage(Demo.hurdle[1], Demo.hurdle2XCord, Demo.hurdle2YCord, null);
       g.drawImage(Demo.hurdle[2], Demo.hurdle3XCord, Demo.hurdle3YCord, null);
       g.drawImage(Demo.hurdle[3], Demo.hurdle4XCord, Demo.hurdle4YCord, null);
-      g.drawImage(imageArray[count], 63, Demo.yCord, null);
+      if(ran.nextInt(20)==4&&Demo.hurdle1XCord>=700&&Demo.hurdle1XCord<=800&&!starVisible&&Demo.isPlayerVisible) {
+        starVisible=true;
+        Demo.starYCord=ran.nextInt(200)+100;
+        Demo.starXCord=1000;
+      }
+      playerRect.x = 63;
+      playerRect.y = Demo.yCord;
+      if(starVisible){
+        g.drawImage(star,Demo.starXCord,Demo.starYCord,null);
+        Demo.starRect.x=Demo.starXCord;
+        Demo.starRect.y=Demo.starYCord;
+      }
+      if(starRect.intersects(playerRect)&&starVisible){
+        starVisible=false;
+        starRect.x=1000;
+        starRect.y=1000;
+        Demo.isPlayerVisible=false;
+      }
+      if(!Demo.isPlayerVisible){
+        g.setColor(Color.RED);
+        g.setFont(font3);
+        counter--;
+        g.drawString("Powered UP : " +counter, 850, 30);
+      }
+      if(counter<=0){
+        Demo.isPlayerVisible=true;
+        counter=60;
+      }
 
+
+      if(ran.nextInt(20)==4&&Demo.hurdle3XCord>=700&&Demo.hurdle3XCord<=800&&!coinDisplay) {
+        Demo.coinYCord=ran.nextInt(200)+100;
+        Demo.coinXCord=1000;
+        whichCoin=ran.nextInt(4);
+        coinDisplay=true;
+        coinShown=coinArray[whichCoin];
+      }
+
+      if(coinDisplay){
+        g.drawImage(coinShown,Demo.coinXCord,Demo.coinYCord,null);
+        Demo.coinRect.x=Demo.coinXCord;
+        Demo.coinRect.y=Demo.coinYCord;
+      }
+
+      if(playerRect.intersects(coinRect)&&coinDisplay){
+        coinDisplay=false;
+        score+=scoreArray[whichCoin];
+      }
+
+      Demo.coinXCord-=20;
+      if(Demo.coinXCord<=-30)
+      {
+        coinDisplay=false;
+      }
+
+
+      if(Demo.isPlayerVisible)
+        g.drawImage(imageArray[count], 63, Demo.yCord, null);
+      else
+        g.drawImage(tImageArray[count], 63, Demo.yCord, null);
 
       if (!Demo.started) {
         g.drawImage(controls, 400, 227, null);
         continue;
       }
-      playerRect.x = 63;
-      playerRect.y = Demo.yCord;
+
 
       if (Demo.hurdle1XCord <= -33 && Demo.hurdle2XCord <= 33) {
         forkOrBranch = ran.nextBoolean();
-        if (forkOrBranch) {
+        if (!forkOrBranch) {
           Demo.hurdle[0] = branch;
           Demo.hurdle[1] = branch;
           hurdle1Rect.height = hurdle2Rect.height = 500;
@@ -235,14 +325,17 @@ public class Demo implements KeyListener {
       hurdle3Rect.y = Demo.hurdle3YCord;
       hurdle4Rect.y = Demo.hurdle4YCord;
 
-      if (playerRect.intersects(hurdle1Rect) || playerRect.intersects(hurdle2Rect) || playerRect.intersects(hurdle3Rect) || playerRect.intersects(hurdle4Rect))
+      if ((playerRect.intersects(hurdle1Rect) || playerRect.intersects(hurdle2Rect) || playerRect.intersects(hurdle3Rect) || playerRect.intersects(hurdle4Rect))&&Demo.isPlayerVisible)
         Demo.lost = true;
 
       g.setColor(Color.BLACK);
       g.setFont(font2);
-      g.drawString("Your Score : " + Demo.score, 400, 50);
+      g.drawString("Your Score : " + Demo.score, 30, 40);
       Demo.score++;
 
+      starXCord-=20;
+      if(starXCord<-40)
+        starVisible=false;
       Demo.yCord += Demo.yVel;
       Demo.yVel += Demo.yAcc;
 
@@ -256,7 +349,7 @@ public class Demo implements KeyListener {
       }
 
       if (Demo.logYCord > -79)
-        Demo.logYCord -= 10;
+        Demo.logYCord -= 20;
       xVal = xVal - 15;
       if (xVal < 0)
         xVal = 1100;
@@ -296,6 +389,7 @@ public class Demo implements KeyListener {
       Demo.hurdle4XCord = 1500;
       Demo.hurdle1YCord = -300;
       Demo.hurdle2YCord = 400;
+
       Demo.hurdle3YCord = -400;
       Demo.hurdle4YCord = 300;
       Demo.hurdle[0] = branch;
@@ -304,6 +398,9 @@ public class Demo implements KeyListener {
       Demo.hurdle[3] = branch;
       Demo.logYCord = 70;
       Demo.score = 0;
+      Demo.isPlayerVisible=true;
+      Demo.coinDisplay=false;
+      Demo.starVisible=false;
       hurdle1Rect = new Rectangle(Demo.hurdle1XCord, Demo.hurdle1YCord, 31, 500);
       hurdle2Rect = new Rectangle(Demo.hurdle2XCord, Demo.hurdle2YCord, 31, 500);
       hurdle3Rect = new Rectangle(Demo.hurdle3XCord, Demo.hurdle3YCord, 31, 500);
